d1891c3aaaecce7f254b8ce1ca15597cf5fe65eb
Jian Tong <tj19832@gmail.com>
2018-08-27 00:02:44 +0800

    try Predicate and BiPredicate

---
+++ src/main/java/com/thoughtworks/java8/lambda/jtong/Player.java
@@ -0,0 +1,24 @@
+package com.thoughtworks.java8.lambda.jtong;
+
+public class Player {
+
+    private final String name;
+
+    public Player(String name, int hp, int ap) {
+
+        this.name = name;
+        this.hp = hp;
+        this.ap = ap;
+    }
+
+    private int hp;
+    private int ap;
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getAp() {
+        return ap;
+    }
+}
